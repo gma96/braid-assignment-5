63bcf74be5f1237b4c5e0e8222996508a3b0b467
Jaffa Panken <jpanken@live.unc.edu>
2016-11-15 04:48:37 +0000

    cleared braid.sh

--- braid.sh
+++
@@ -1,8 +0,0 @@
-USE script;
-CREATE TABLE script (Name VARCHAR(255),
-    -> City VARCHAR(255),
-    -> Siblings INT,
-    -> Age INT,
-    -> Major VARCHAR(255),
-    -> ID VARCHAR(255),
-    -> Date DATE);
