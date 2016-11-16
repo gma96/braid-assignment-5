abce55704b2866732d49e5e22a03946c33402993
Jaffa Panken <jpanken@live.unc.edu>
2016-11-16 21:56:36 +0000

    tried commands in braid0.sh

--- braid0.sh
+++ braid0.sh
@@ -28,4 +28,6 @@ ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 DATE=$(date +"%Y-%m-%d")	
 
 # Prints answers, ID, and Date to csv file
-echo $A1, $A2, $A3, $A4, $A5, $ID, $DATE >> answers.csv
+echo $A1, $A2, $A3, $A4, $A5, $ID, $DATE >> answer.csv
+
+mysql -u jpanken -p -e "USE script; INSERT INTO script (Name, City, Siblings, Age, Major, ID, Date) VALUES ($A1, $A2, $A3, $A4, $A5, $ID, $DATE)"
