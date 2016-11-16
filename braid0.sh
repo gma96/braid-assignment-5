490cccf905e0885cec8d15e1baf66aef9daa1697
Todd <tbowser@live.unc.edu>
2016-11-16 22:17:20 +0000

    needless changes to csv

--- braid0.sh
+++ braid0.sh
@@ -25,9 +25,7 @@ read A5
 ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
 
 # Date
-DATE=$(date +"%m-%d-%Y")	
+DATE=$(date +"%Y-%m-%d")	
 
 # Prints answers, ID, and Date to csv file
-echo $A1, $A2, $A3, $A4, $A5, $ID, $DATE >> braid.sql
-
-mysqldump -u jpanken -p script < braid0.sql
+echo $A1, $A2, $A3, $A4, $A5, $ID, $DATE >> answers.csv
