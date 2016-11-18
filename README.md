4c83f1049174552b1f20db06ad9324269d5e3f5e
Todd <tbowser@live.unc.edu>
2016-11-18 00:10:47 +0000

    modified README with guidelines

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # braid-assignment-5
+
+Users add data by running braid0.sh
+
+Database Master can enter data through braid2.sh; passwords are unspecified - 
+hit 'enter' when prompted
+
+Database Master manually pulls data from target file braid1.sql
+into database 'script' 
+
+
--- braid1.sql
+++ braid1.sql
@@ -4,3 +4,4 @@ Emma, Charleston, 3, 45, chemistry, SmycaI, 2016-11-17
 Matt, NYC, 0, 35, Law, MvXECO, 2016-11-17
 Hal, Newton, 2, 17, Literature, gGStr8, 2016-11-17
 Ralph, Detroit, 2, 21, Urban Planning, uMxh4q, 2016-11-17
+Ezra, Kernersville, 1, 31, Electronics, bZJ6bB, 2016-11-18
